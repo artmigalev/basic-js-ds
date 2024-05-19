50a891aa1e7164f67fcbd787e7616c4475b82284
Artyom Migalev <art_migalev@yahoo.com>
2024-05-19 11:19:47 +0300

    add method add and root

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,21 +1,37 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor(){
+    this.root = null;
 
+  }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(!data) return null
+    return this.root;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data ) {
+    let newNode = new Node(data)
+    if (this.root === null) this.root = newNode;
+    else this.insertNode(this.root, newNode);
+  }
+  insertNode(node, newNode) {
+    if (newNode.data < node.data){
+      if(node.left === null){
+        node.left = newNode
+      }else this.insertNode(node.left,newNode)
+    }else {
+      if(node.right === null){
+        node.right = newNode
+      }else this.insertNode(node.right,newNode)
+    }
+
   }
 
   has(/* data */) {
